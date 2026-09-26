cde903e18d98717b9f2efbf492f87984a34efdda
imranhashmish1 <imrankhanhashmi301@gmail.com>
2026-09-26 07:05:48 +0430

    Update referral.js
    Update 3 level referral system

--- api/referral.js
+++ api/referral.js
@@ -3,10 +3,7 @@ export default async function handler(req, res) {
     return res.status(405).json({
       ok: false,
       message: "Method not allowed"
-    });
-  }
-
-  try {
+    })allowed  try {
     const SUPABASE_URL = process.env.SUPABASE_URL;
     const SUPABASE_SECRET_KEY = process.env.SUPABASE_SECRET_KEY;
 
